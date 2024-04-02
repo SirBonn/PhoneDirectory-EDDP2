fcfd22e467ce5ac465b328631e97462fb1664a82
SirBonn <byronvasquez21@gmail.com>
2024-04-02 11:46:07 -0600

    update 3

--- main.cpp
+++ main.cpp
@@ -4,13 +4,12 @@
 #include "string"
 #include "directory/Group.h"
 #include "structures/Hash/TableLL.h"
-#include "directory/Fields/Field.h"
-#include "directory/Fields/FieldInt.h"
-#include "directory/Fields/FieldChar.h"
-#include "directory/Fields/FieldString.h"
+#include "directory/Field.h"
+#include "structures/tree/TreeInteger.h"
+#include "structures/tree/TreeString.h"
+#include "structures/tree/TreeChar.h"
+#include "utils/Export/DataManager.h"
 
-
-class FieldString;
 
 int main() {
 
@@ -19,11 +18,14 @@ int main() {
 
     while (!exit) {
         std::string entrada;
+        std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n ";
         std::cout << "Ingrese el comando: ";
         std::getline(std::cin, entrada);
         std::replace(entrada.begin(), entrada.end(), '(', ' ');
         std::replace(entrada.begin(), entrada.end(), ')', ' ');
+        std::replace(entrada.begin(), entrada.end(), '=', ' ');
         std::replace(entrada.begin(), entrada.end(), ',', ' ');
+        std::replace(entrada.begin(), entrada.end(), ';', ' ');
         std::istringstream iss(entrada);
         std::string action, sentence, groupName, token;
         iss >> action >> sentence >> groupName;
@@ -34,17 +36,17 @@ int main() {
 
             while (iss >> token) {
                 if (token == "FIELDS") {
-                    while (iss >> token && token != ");") {
+                    while (iss >> token) {
                         std::string nombreCampo, tipoDato;
                         iss >> tipoDato;
                         if (tipoDato == "STRING" || tipoDato == "DATE") {
-                            FieldString campo(token, tipoDato);
+                            Field *campo = new Field(token, tipoDato);
                             newGroup->addField(campo);
                         } else if (tipoDato == "INTEGER") {
-                            FieldInt campo(token, tipoDato);
+                            Field *campo = new Field(token, tipoDato);
                             newGroup->addField(campo);
                         } else if (tipoDato == "CHAR") {
-                            FieldChar campo(token, tipoDato);
+                            Field *campo = new Field(token, tipoDato);
                             newGroup->addField(campo);
                         }
                     }
@@ -56,43 +58,149 @@ int main() {
             std::cout << "Cantidad de campos: " << newGroup->getCuantityFields() << std::endl;
             table->insert(newGroup);
 
+
         } else if (action == "ADD" && sentence == "CONTACT") {
 
             iss >> token;
             std::string nombreContacto = token;
             Group *groupSearched = table->searchKey(nombreContacto);
             SimpleNode<Field> *node = groupSearched->getFields()->gethead();
-
+            std::string content = "", firstField = "";
+            iss >> token;
             if (token == "FIELDS") {
-                while (iss >> token && token != ");") { //token pasa a ser el valor del campo
+                TableLL<TreeAVL> *fieldTable = groupSearched->getTableAVL();
+                while (iss >> token && token != ");") {
+                    content += token + ", ";
+                    if (firstField == "") {
+                        firstField = token;
+                    }
 
                     while (node != nullptr) {
 
                         Field *field = node->getData();
-                        TreeAVL<Field> *tree = groupSearched->getTableAVL()->searchKey(field->getKey());
-                        tree->insert(token);
-                        node = node->getPtr();
+                        if (field->getFieldType() == "STRING") {
 
+                            TreeString *treeas = static_cast<TreeString *>(fieldTable->searchKey(
+                                    field->getKey()));
+                            treeas->insert(new std::string(token));
+
+                        } else if (field->getFieldType() == "INTEGER") {
+                            int num;
+                            std::istringstream(token) >> num;
+                            TreeInteger *treeas = static_cast<TreeInteger *>(fieldTable->searchKey(
+                                    field->getKey()));
+                            treeas->insert(new int(num));
+                        } else if (field->getFieldType() == "CHAR") {
+//
+                            TreeChar *treeas = static_cast<TreeChar *>(fieldTable->searchKey(
+                                    field->getKey()));
+                            treeas->insert(new char(token[0]));
+                        }
+                        node = node->getPtr();
                     }
-                    TableLL<TreeAVL<Field>> *tableAvl = table->searchKey(nombreContacto)->getTableAVL();
+
+                }
+            }
+            DataManager::createFolder(nombreContacto, content, firstField);
+        } else if (action == "FIND" && sentence == "CONTACT") {
+
+            iss >> token;
+            std::string nombreContacto = token;
+            std::string *key;
+            Group *groupSearched = table->searchKey(nombreContacto);
+            SimpleNode<Field> *node = groupSearched->getFields()->gethead();
+            iss >> token;
+            if (token == "CONTACT-FIELD") {
+                TableLL<TreeAVL> *fieldTable = groupSearched->getTableAVL();
+                while (iss >> token && token != ");") { //token pasa a ser el valor del campo
+                    std::string value;
+                    iss >> value;
+                    while (node != nullptr) {
+
+                        Field *field = node->getData();
+
+                        if (field->getKey() == token) {
+                            if (field->getFieldType() == "STRING") {
+
+                                TreeString *treeas = static_cast<TreeString *>(fieldTable->searchKey(
+                                        field->getKey()));
+
+                                key = treeas->searchValueKey(&value);
+
+                            } else if (field->getFieldType() == "INTEGER") {
+                                int num;
+                                std::istringstream(value) >> num;
+                                TreeInteger *treeas = static_cast<TreeInteger *>(fieldTable->searchKey(
+                                        field->getKey()));
+                                key = treeas->searchValueKey(&num);
+
+                            } else if (field->getFieldType() == "CHAR") {
+                                //
+                                TreeChar *treeas = static_cast<TreeChar *>(fieldTable->searchKey(
+                                        field->getKey()));
+                                key = treeas->searchValueKey(&value[0]);
+                            }
+
+                        }
+                        node = node->getPtr();
+                    }
+
+                    SimpleNode<Field> *node = groupSearched->getFields()->gethead();
+
+                    TableLL<TreeAVL> *fieldTable = groupSearched->getTableAVL();
+
+                    while (node != nullptr) {
+
+                        Field *field = node->getData();
+                        if (field->getFieldType() == "STRING") {
+
+                            TreeString *treeas = static_cast<TreeString *>(fieldTable->searchKey(
+                                    field->getKey()));
+                            std::cout << field->getKey() << ": " << treeas->searchkey(key)->getData() << std::endl;
+
+                        } else if (field->getFieldType() == "INTEGER") {
+                            int num;
+                            std::istringstream(token) >> num;
+                            TreeInteger *treeas = static_cast<TreeInteger *>(fieldTable->searchKey(
+                                    field->getKey()));
+                            std::cout << field->getKey() << ": " << treeas->searchkey(key)->getData() << std::endl;
+                        } else if (field->getFieldType() == "CHAR") {
+//
+                            TreeChar *treeas = static_cast<TreeChar *>(fieldTable->searchKey(
+                                    field->getKey()));
+                            std::cout << field->getKey() << ": " << treeas->searchKey(key)->getData() << std::endl;
+                        }
+                        node = node->getPtr();
+                    }
 
                 }
             }
 
-
-        } else if (action == "FIND" && sentence == "CONTACT") {
-
-            std::cout << "Bsucando contacto." << std::endl;
-
+        } else if (action == "GRAPH" && (groupName == "TABLES" || groupName == "TABLE")) {
+            if (sentence == "ALL") {
+                table->printHashtable(table);
+            } else {
+                Group *groupSearched = table->searchKey(sentence);
+                if (groupSearched == NULL) {
+                    std::cout << "No se encontro el grupo." <<
+                              std::endl;
+                } else {
+                    GraphvizManager *graphvizManager = new GraphvizManager(groupSearched->getKey());
+                    groupSearched->printGroup(graphvizManager);
+                    graphvizManager->generateGraph();
+                }
+            }
+            table->printHashtable(table);
         } else if (action == "EXIT" || action == "exit") {
             exit = true;
             return 0;
         } else {
-            std::cout << "Comando no reconocido." << std::endl;
+            std::cout << "Comando no reconocido." <<
+                      std::endl;
         }
 
-//        std::cout << "Presione una tecla para continuar...";
-//        getch();
+        std::cout << "Presione una tecla para continuar...";
+        getch();
 
     }
 
